ee602c3792d9323772db6fbec1e19a2b2ddfbe26
unknown <depeters@FNB-GRPTEST-VM.mndeeddom.deed.state.mn.us>
2014-07-03 14:19:01 -0500

    change to the directive

--- EmployerSearchTokenizerDirective.js
+++ EmployerSearchTokenizerDirective.js
@@ -10,58 +10,59 @@ angular.module("directives.employerSearchTokenizerDirective", []).directive(
 					// reset input field dirty and valid values
 					// eval("scope."+attrs.propertyName + ".dirty = false");
 
-					var tokenInput = document.createElement("input");
-					tokenInput.type = "text";
-					tokenInput.className = "form-control";
-					tokenInput.id = "tokenfield";
-					tokenInput.value = "test";
+//					var tokenInput = document.createElement("input");
+//					tokenInput.type = "text";
+//					tokenInput.className = "form-control";
+//					tokenInput.id = "tokenfield";
+//					tokenInput.value = "test";
+					var myHelloElement = document.createElement("div");
+					myHelloElement.innerHTML = "Hello Everyone.  I should be a cool employer search directive.  Instead, I'm just this disappointing text";
+					element.replaceWith(myHelloElement);
 
-					element.replaceWith(tokenInput);
-
-					var prePopulate = eval("scope."+attrs.propertyName); 
-
-					if (prePopulate && prePopulate.length > 0) {
-						$(tokenInput).tokenInput(attrs.url, {
-							theme : "facebook",
-							onAdd : function(item) {
-								eval("scope."+attrs.propertyName + ".push(item);");
-								
-								scope.$apply();
-							},
-							onDelete : function(item) {
-								var i = eval("scope."+attrs.propertyName + ".indexOf(item);");
-								if(i != -1)
-								{
-									eval("scope."+attrs.propertyName + ".splice(i,1);");
-								}
-								scope.$apply();
-							},
-							tokenLimit : attrs.allowedTokens,
-							prePopulate : prePopulate
-						});
-					} else {
-						$(tokenInput).tokenInput(attrs.url, {
-							theme : "facebook",
-							onAdd : function(item) {
-								
-								eval("scope."+attrs.propertyName + ".push(item);");
-								
-								scope.$apply();
-							},
-							onDelete : function(item) {
-								
-								
-								var i = eval("scope."+attrs.propertyName + ".indexOf(item);");
-								if(i != -1)
-								{
-									eval("scope."+attrs.propertyName + ".splice(i,1);");
-								}
-								scope.$apply();
-							},
-							tokenLimit : attrs.allowedTokens
-						});
-					}
-					$("ul.token-input-list-facebook").css("width","650px");
+//					var prePopulate = eval("scope."+attrs.propertyName); 
+//
+//					if (prePopulate && prePopulate.length > 0) {
+//						$(tokenInput).tokenInput(attrs.url, {
+//							theme : "facebook",
+//							onAdd : function(item) {
+//								eval("scope."+attrs.propertyName + ".push(item);");
+//								
+//								scope.$apply();
+//							},
+//							onDelete : function(item) {
+//								var i = eval("scope."+attrs.propertyName + ".indexOf(item);");
+//								if(i != -1)
+//								{
+//									eval("scope."+attrs.propertyName + ".splice(i,1);");
+//								}
+//								scope.$apply();
+//							},
+//							tokenLimit : attrs.allowedTokens,
+//							prePopulate : prePopulate
+//						});
+//					} else {
+//						$(tokenInput).tokenInput(attrs.url, {
+//							theme : "facebook",
+//							onAdd : function(item) {
+//								
+//								eval("scope."+attrs.propertyName + ".push(item);");
+//								
+//								scope.$apply();
+//							},
+//							onDelete : function(item) {
+//								
+//								
+//								var i = eval("scope."+attrs.propertyName + ".indexOf(item);");
+//								if(i != -1)
+//								{
+//									eval("scope."+attrs.propertyName + ".splice(i,1);");
+//								}
+//								scope.$apply();
+//							},
+//							tokenLimit : attrs.allowedTokens
+//						});
+//					}
+//					$("ul.token-input-list-facebook").css("width","650px");
 				}
 			};
 
